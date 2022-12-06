b87082c2480f3983343227a6207b9ba0dd44e793
SoramWanglen <soramwanglen.net@gmail.com>
2022-12-06 11:21:09 +0530

    mvc files added

---
+++ MyFirstMVC/Controllers/PFolioController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVC.Controllers
+{
+    public class PFolioController : Controller
+    {
+        // GET: PFolio
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Soram()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Hobby()
+        {
+            return View();
+        }
+    }
+}
